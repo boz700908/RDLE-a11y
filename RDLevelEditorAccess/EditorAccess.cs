c5f88f1651bae678e739134e64c21d9fb0bd13d0
hzt <huangzitong94@gmail.com>
2026-03-12 23:00:28 +0800

    更新到1.3.1

--- RDLevelEditorAccess/EditorAccess.cs
+++ RDLevelEditorAccess/EditorAccess.cs
@@ -15,7 +15,7 @@ namespace RDLevelEditorAccess
     // ===================================================================================
     // 第一部分：加载器 (Loader)
     // ===================================================================================
-    [BepInPlugin("com.hzt.rd-editor-access", "RDEditorAccess", "1.3")]
+    [BepInPlugin("com.hzt.rd-editor-access", "RDEditorAccess", "1.31")]
     public class EditorAccess : BaseUnityPlugin
     {
         public void Awake()
